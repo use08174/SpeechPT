10cb3d77d5bad5d46385837ab79cf693b00c8d67
use08174 <use08174@gmail.com>
2024-01-31 21:57:29 +0900

    feat: add files for api test

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,15 +8,7 @@ from func import analyze_file
 
 app = FastAPI(project_name='speech_practice')
 
-# app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="frontend/templates")
-
-
-@app.get('/', response_class=HTMLResponse)  # connect the file received form the frontend
-async def root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request, "name": "Rahul"})
-
-
 @app.post("/detect/")
 async def analyze_endpoint(file: UploadFile):
     return await analyze_file(file)
+
